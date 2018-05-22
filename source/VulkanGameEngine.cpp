507398ba43fc73a945b7a09793e7da2905aa2fa3
Lauri Kortevaara <lauri.kortevaara@gmail.com>
2018-05-22 11:33:49 +0300

    Included GLM and GLFW into CMake-project as well as tested glfwInit() in VulkanGameEngine.cpp

--- source/VulkanGameEngine.cpp
+++ source/VulkanGameEngine.cpp
@@ -1,8 +1,12 @@
 #include "VulkanGameEngine.hpp"
 
+#include <GLFW/glfw3.h>
 #include <iostream>
 
 VulkanGameEngine::VulkanGameEngine()
 {
 	std::cout << "Vulkan Game Engine instance created" << std::endl;
+	int res = glfwInit();
+	if (res != GLFW_TRUE)
+		throw(std::runtime_error("Could not initialize GLFW!"));
 }
